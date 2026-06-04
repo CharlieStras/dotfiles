2c831b67afc466f42b53e239fb742e0905b0b42e
charliestras <charliestras@gmail.com>
2026-06-04 22:03:59 +0800

    feat(config/nvim): add nvim-treesitter and LSP plugins with lazydev

---
+++ config/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").setup {
+	install_dir = vim.fn.stdpath("data") .. "/site"
+      }
+      require("nvim-treesitter").install {
+	"lua",
+	"java",
+	"scala",
+	"markdown",
+	"markdown_inline"
+      }
+    end,
+  }
+}
