157de704d07b8cc79a90090414dc30aae54f9ac0
charliestras <charliestras@gmail.com>
2026-06-04 22:04:05 +0800

    feat(config/nvim): configure diagnostics, treesitter autocmds and editorconfig

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -8,3 +8,28 @@ vim.opt.clipboard = "unnamedplus"
 vim.keymap.set("n", "<Space><Space>x", "<Cmd>source %<CR>")
 vim.keymap.set("n", "<Space>x", ":.lua<CR>")
 vim.keymap.set("v", "<Space>x", ":lua<CR>")
+
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+	float = {
+		border = "rounded",
+		source = true,
+	},
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "lua", "java", "scala", "markdown", "makrdown_inline" },
+	callback = function()
+		-- syntax highlighting, provided by Neovim
+		vim.treesitter.start()
+		-- folds, provided by Neovim
+		vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+		vim.wo[0][0].foldmethod = "expr"
+		-- indentation, provided by nvim-treesitter
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+	end,
+})
